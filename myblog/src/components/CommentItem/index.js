1f310ed4fe40c9838f4dda3832e6c62914265f52
wuwzhang@gmail.com <wuwzhang@gmail.com>
2017-08-30 14:57:47 +0800

    Comment marked

--- myblog/src/components/CommentItem/index.js
+++ myblog/src/components/CommentItem/index.js
@@ -4,13 +4,24 @@ import {
   Col
 } from 'react-bootstrap'
 
+const marked = require('marked');
+
 const CommentItem = ({user, content, create_at}) => {
   create_at = create_at.slice(0, 10);
   return (
     <Row>
       <li>
         <Col md={2}><span>{ user.username }: </span></Col>
-        <Col md={8}><span>{ content }</span></Col>
+        <Col md={8}>
+          {
+           content ? <span
+                      dangerouslySetInnerHTML={{
+                        __html: marked(content, {sanitize: true})
+                      }}
+                     ></span>
+                   : null
+          }
+        </Col>
         <Col md={2}><span>{ create_at }</span></Col>
       </li>
     </Row>
@@ -19,3 +30,4 @@ const CommentItem = ({user, content, create_at}) => {
 }
 
 export default CommentItem;
+
