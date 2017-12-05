75ed47ef3f22fbe457462742320cdba83b3737c0
wuwZhang <wuwZhang@gmail.com>
2017-12-05 20:08:35 +0800

    Add catalog_count redis

--- myblog/src/components/Comment/reducer.js
+++ myblog/src/components/Comment/reducer.js
@@ -11,7 +11,7 @@ import {
   SET_THUMBSUP,
   SET_THUMBSDOWN,
   SET_REPORT
-} from './actionType.js';
+} from "./actionType.js";
 
 export default (state, action) => {
   if (!state) {
@@ -19,62 +19,61 @@ export default (state, action) => {
       articleComments: [],
       allComment: [],
       NotCheckedCount: 0,
-      filter: 'ALL'
-    }
+      ReportCount: 0,
+      filter: "ALL"
+    };
   }
 
   switch (action.type) {
     case INIT_COMMENT: {
       return {
         ...state,
-        articleComments: action.comments.map((comment) => {
+        articleComments: action.comments.map(comment => {
           return {
             ...comment,
             isShowReply: false
-          }
+          };
         })
-      }
+      };
     }
     case INIT_ALL_COMMENT: {
       return {
         ...state,
         allComment: action.comments
-      }
+      };
     }
     case ADD_COMMENT: {
       return {
         ...state,
-        articleComments: [
-          action.comment,
-          ...state.articleComments
-        ],
+        articleComments: [action.comment, ...state.articleComments],
         NotCheckedCount: state.NotCheckedCount + 1
-      }
+      };
     }
     case ADD_SUBCOMMENT: {
-
       return {
         ...state,
         articleComments: [
-          ...state.articleComments[action.commentIndex].replies.push(action.subComment),
+          ...state.articleComments[action.commentIndex].replies.push(
+            action.subComment
+          ),
           ...state.articleComments
         ]
-      }
+      };
     }
     case CHECK_COMMENT: {
       return {
         ...state,
         allComment: [
-          ...state.allComment[action.commentIndex].isChecked = true,
+          ...(state.allComment[action.commentIndex].isChecked = true),
           ...state.allComment
         ],
         NotCheckedCount: state.NotCheckedCount - 1
-      }
+      };
     }
     case DELETE_COMMENT: {
-
-      let count = action.isChecked? state.NotCheckedCount
-                                  : state.NotCheckedCount - 1
+      let count = action.isChecked
+        ? state.NotCheckedCount
+        : state.NotCheckedCount - 1;
 
       return {
         ...state,
@@ -83,28 +82,29 @@ export default (state, action) => {
           ...state.allComment.slice(action.commentIndex + 1)
         ],
         NotCheckedCount: count
-      }
+      };
     }
     case NOTCHECKED_COMMENT: {
       return {
         ...state,
         NotCheckedCount: action.NotCheckedCount
-      }
+      };
     }
     case SET_COMMENT_FILTER: {
       return {
         ...state,
         filter: action.filter
-      }
+      };
     }
     case SET_SHOW_REPLY: {
       return {
         ...state,
         articleComments: [
-          ...state.articleComments[action.commentIndex].isShowReply = action.state,
+          ...(state.articleComments[action.commentIndex].isShowReply =
+            action.state),
           ...state.articleComments
         ]
-      }
+      };
     }
     case SET_THUMBSUP: {
       let likesState = action.state;
@@ -112,11 +112,15 @@ export default (state, action) => {
       return {
         ...state,
         articleComments: [
-          ...state.articleComments[action.commentIndex].likesState = likesState,
-          ...state.articleComments[action.commentIndex].thumbsUp += likesState,
+          ...(state.articleComments[
+            action.commentIndex
+          ].likesState = likesState),
+          ...(state.articleComments[
+            action.commentIndex
+          ].thumbsUp += likesState),
           ...state.articleComments
         ]
-      }
+      };
     }
     case SET_THUMBSDOWN: {
       let dislikesState = action.state;
@@ -124,23 +128,27 @@ export default (state, action) => {
       return {
         ...state,
         articleComments: [
-          ...state.articleComments[action.commentIndex].dislikesState = dislikesState,
-          ...state.articleComments[action.commentIndex].thumbsDown += dislikesState,
+          ...(state.articleComments[
+            action.commentIndex
+          ].dislikesState = dislikesState),
+          ...(state.articleComments[
+            action.commentIndex
+          ].thumbsDown += dislikesState),
           ...state.articleComments
         ]
-      }
+      };
     }
     case SET_REPORT: {
       return {
         ...state,
         articleComments: [
-          ...state.articleComments[action.commentIndex].report = action.state,
+          ...(state.articleComments[action.commentIndex].report = action.state),
           ...state.articleComments
         ]
-      }
+      };
     }
     default: {
       return state;
     }
   }
-}
+};
