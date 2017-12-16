45e2d5a3e89bb03f33140cf3775db7cbb7a0f414
wuwZhang <wuwZhang@gmail.com>
2017-12-16 21:04:43 +0800

    Fix Article-Edit Tags And Catalogs Post

--- server/lib/tags.js
+++ server/lib/tags.js
@@ -4,26 +4,60 @@ const Tags = require("../models/").Tags;
 exports.addTags = (tags, articleId) => {
   // console.log("tags", tags);
   tags.forEach(async tag => {
-    await Tags.findOne({ tag: tag }).then(async (res, err) => {
-      if (err) {
-        throw new Error(err);
-      }
-      if (res) {
-        return Tags.update(
-          { tag: tag },
-          { $addToSet: { articleId: articleId } }
-        );
-      } else {
-        return Tags.create({ tag, articleId });
-      }
-    });
+    try {
+      await Tags.findOne({ tag: tag }).then(async (res, err) => {
+        if (err) {
+          throw new Error(err);
+        }
+        if (res) {
+          return Tags.update(
+            { tag: tag },
+            { $addToSet: { articleId: articleId } }
+          );
+        } else {
+          return Tags.create({ tag, articleId });
+        }
+      });
+    } catch (e) {
+      throw new Error(e);
+    }
   });
 };
 exports.findTagByTagName = tag => {
   return Tags.findOne({ tag: tag }).exec();
 };
-exports.deleteTag = tagId => {
-  return Tags.remove({ _id: tagId }).exec();
+exports.deleteTags = (tags, articleId) => {
+  tags.forEach(async tag => {
+    await Tags.update({ tag: tag }, { $pull: { articleId: articleId } }).then(
+      async res => {
+        if (res.ok != 1) {
+          return false;
+        }
+        let ans = await Tags.aggregate(
+          { $match: { tag: tag } },
+          {
+            $addFields: {
+              count: {
+                $size: "$articleId"
+              }
+            }
+          },
+          {
+            $project: {
+              count: 1
+            }
+          }
+        );
+
+        if (ans[0].count === 0) {
+          console.log("remove tags", tags);
+          return await Tags.remove({ tag: tag });
+        } else {
+          return true;
+        }
+      }
+    );
+  });
 };
 exports.getTags = count => {
   return Tags.find()
@@ -32,4 +66,13 @@ exports.getTags = count => {
 };
 exports.getAllTags = () => {
   return Tags.find().exec();
+};
+
+exports.updateTag = (tags, articleId) => {
+  tags.forEach(async tag => {
+    try {
+    } catch (e) {
+      throw new Error(e);
+    }
+  });
 };
