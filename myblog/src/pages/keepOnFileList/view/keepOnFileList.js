120159480fc943bcb97d727a9c8dbd1ceb2cba4c
wuwZhang <wuwZhang@gmail.com>
2017-12-26 23:11:52 +0800

    Fix KeepOnFile Page Layout

--- myblog/src/pages/keepOnFileList/view/keepOnFileList.js
+++ myblog/src/pages/keepOnFileList/view/keepOnFileList.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import { getArticleDateList } from "../fetch.js";
+import {
+  fetchs as configFetchs,
+  actions as configActions
+} from "../../SettingAdmin/";
 
 import { view as KeepOnFileItem } from "../../../components/KeepOnFileItem/";
 import KeepOnFile from "../../../components/KeepOnFile/index.js";
@@ -11,7 +17,7 @@ import { view as Rank } from "../../../components/Rank/";
 import Footer from "../../../components/Footer/index.js";
 import Pagination from "../../../components/Pagination/pagination";
 
-import { Timeline, Radio, Col, Row } from "antd";
+import { Timeline, Radio, Col, Row, Icon, Spin } from "antd";
 import FontAwesome from "react-fontawesome";
 import { FormattedMessage } from "react-intl";
 import QueueAnim from "rc-queue-anim";
@@ -21,20 +27,45 @@ class KeepOnFileList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      searchLoading: true,
       articleList: [],
-      catalogView: "catalog"
+      catalogView: "catalog", //true 为list视图, false为summary视图
+      aside: true //true为base， false为rank
     };
     this.handleView = this.handleView.bind(this);
     this.handlePage = this.handlePage.bind(this);
   }
   async componentDidMount() {
+    let ans = localStorage.getItem("config"),
+      config = JSON.parse(ans);
+    if (!config) {
+      let res = await configFetchs.getConfig();
+
+      if (res.code === "1") {
+        this.props.initConfig(res.config);
+        localStorage.setItem("config", JSON.stringify(res.config));
+        let keepOnFileConfig = res.config.keepOnFile;
+        this.setState({
+          catalogView: keepOnFileConfig.catalogView,
+          aside: keepOnFileConfig.aside
+        });
+      }
+    } else {
+      let keepOnFileConfig = config.keepOnFile;
+      this.setState({
+        catalogView: keepOnFileConfig.catalogView,
+        aside: keepOnFileConfig.aside
+      });
+    }
+
     let result = await getArticleDateList(1, 4);
 
     if (result.code === "1") {
       this.setState({
         articleList: result.articles,
         currentPage: 1,
-        count: result.count
+        count: result.count,
+        searchLoading: false
       });
     } else {
       console.log(result);
@@ -59,13 +90,21 @@ class KeepOnFileList extends Component {
     }
   }
 
+  handleAside(e) {
+    e.preventDefault();
+    let { aside } = this.state;
+    this.setState({
+      aside: !aside
+    });
+  }
+
   handleView(e) {
     this.setState({
       catalogView: e.target.value
     });
   }
   render() {
-    let { articleList, catalogView, currentPage, count } = this.state;
+    let { articleList, catalogView, aside, currentPage, count } = this.state;
     let totalPages = Math.ceil(count / 4);
 
     return (
@@ -98,44 +137,63 @@ class KeepOnFileList extends Component {
             </Col>
             <Row>
               <Col md={20} sm={20} xs={24}>
-                <section className="timeline">
-                  <Timeline>
-                    <QueueAnim>
-                      {articleList.map((article, index) => {
-                        return article ? (
-                          <Timeline.Item key={index}>
-                            <KeepOnFileItem
-                              key={index}
-                              article={article}
-                              catalogView={
-                                catalogView === "catalog" || !catalogView
-                                  ? true
-                                  : false
-                              }
-                            />
-                          </Timeline.Item>
-                        ) : null;
-                      })}
-                    </QueueAnim>
-                  </Timeline>
-                  <Pagination
-                    totalPages={totalPages}
-                    currentPage={currentPage}
-                    range={5}
-                    onChange={this.handlePage}
-                  />
-                </section>
+                <Spin size="large" spinning={this.state.searchLoading === true}>
+                  <section className="timeline">
+                    <Timeline>
+                      <QueueAnim>
+                        {articleList.map((article, index) => {
+                          return article ? (
+                            <Timeline.Item key={index}>
+                              <KeepOnFileItem
+                                key={index}
+                                article={article}
+                                catalogView={catalogView}
+                              />
+                            </Timeline.Item>
+                          ) : null;
+                        })}
+                      </QueueAnim>
+                    </Timeline>
+                  </section>
+                </Spin>
+                <Pagination
+                  totalPages={totalPages}
+                  currentPage={currentPage}
+                  range={5}
+                  onChange={this.handlePage}
+                />
               </Col>
               <Col md={4} sm={4} xs={0}>
-                <section className="aside">
-                  <div>
-                    <SearchBox color="#07689f" />
-                    <KeepOnFile color="#07689f" badgeColor="#FF7E67" />
-                    <TagsCloud color="#07689f" />
-                    <CatalogAside color="#07689f" />
-                  </div>
+                <section className="keepOnFile-aside">
+                  {aside ? (
+                    <section className="aside">
+                      <SearchBox color="#07689f" />
+                      <KeepOnFile color="#07689f" badgeColor="#FF7E67" />
+                      <TagsCloud color="#07689f" />
+                      <CatalogAside color="#07689f" />
+                    </section>
+                  ) : (
+                    <section className="keepOnFile-rank">
+                      <Rank />
+                    </section>
+                  )}
+                  <p
+                    className="keepOnFile-aside-change-btn"
+                    onClick={e => this.handleAside(e)}
+                  >
+                    {aside ? (
+                      <p>
+                        <Icon type="double-right" className="right-animtion" />
+                        <FormattedMessage id="Rank" defaultMessage="Rank" />
+                      </p>
+                    ) : (
+                      <p>
+                        <Icon type="double-left" className="left-animtion" />
+                        <FormattedMessage id="Base" defaultMessage="Base" />
+                      </p>
+                    )}
+                  </p>
                 </section>
-                <Rank style={{ padding: "20px" }} />
               </Col>
             </Row>
           </section>
@@ -146,4 +204,16 @@ class KeepOnFileList extends Component {
   }
 }
 
-export default KeepOnFileList;
+const mapStateToProps = state => ({
+  config: state.blog.config
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    initConfig: config => {
+      dispatch(configActions.initConfig(config));
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(KeepOnFileList);
