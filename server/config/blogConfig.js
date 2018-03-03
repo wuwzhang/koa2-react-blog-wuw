474972bdf19cc7d60570b9ab1775fa7b01847095
wuwZhang <wuwZhang@gmail.com>
2018-03-03 23:37:24 +0800

    Add Server Build Flolder

---
+++ server/config/blogConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+  searchPage: { base: true, rank: true, articleCount: 4, rankCount: 4 },
+  keepOnFile: {
+    aside: true,
+    catalogView: "catalog",
+    rankCount: 5,
+    articleCount: 4
+  },
+  articleDetails: { base: true, rank: true, rankCount: 4, mathJax: true },
+  comment: { count: 4, subComment: 5 },
+  basic: {
+    NewMessageSendToEmail: true,
+    NewCommentSendToEmail: true,
+    CommentReportSendToEmail: true
+  }
+};
---
+++ server/config/default.js
@@ -0,0 +1,23 @@
+module.exports = {
+  port: 3008,
+  secretKey: 'wuw',
+  session: {
+    secret: 'myblog',
+    key: 'myblog',
+    maxAge: 2592000000
+  },
+  mongodb: 'mongodb://localhost:27017/myblog',
+  redis: {
+    port: 6379,
+    host: 'localhost',
+    auth: 'wuw',
+    db: 6,
+    family: 4
+  },
+  github: {
+    'client_id': 'b3b9f407b0d5ad50b6ec',
+    'client_secret': '8691d9c926b971ac02356f5233e266fdc1b07a37',
+    'scope': ['user']
+  },
+  cors: 'http://localhost:3000'
+}
